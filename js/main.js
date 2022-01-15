e619fbb76e2992ff89697afeaf149dd8932a9c3e
zklob <76848217+zklob@users.noreply.github.com>
2022-01-15 13:36:45 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#btn").html("Download ("+size+")");
+	$("#size").html(size);
+	$("title").html(name);
+	$(".filename").html(name+'.'+type);
+	$(".file-name").html(name);
+});
+
+
